c0198493c47415898de4a2ce1381c4d312cfe5a0
Stefan Haslinger <stefan.haslinger@mittenin.at>
2014-03-26 13:16:49 +0100

    typo

--- app/models/mercator_mesonic/order.rb
+++ app/models/mercator_mesonic/order.rb
@@ -118,8 +118,7 @@ module MercatorMesonic
                C160: 0,
                mesocomp: AktMandant.mesocomp,
                mesoyear: AktMandant.mesoyear,
-               mesoprim: c000 + "-" + AktMandant.mesocomp + "-" + AktMandant.mesoyear )
-      end
+               mesoprim: kontonummer + "-" + custom_order_number + "-" + AktMandant.mesocomp + "-" + AktMandant.mesoyear )
     end
 
     # --- Instance Methods --- #
