562f486eb99dfef97211266144a25170fd8db070
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-03-13 18:27:16 +0100

    first part of photo import

--- app/models/mercator_mesonic/bild.rb
+++ app/models/mercator_mesonic/bild.rb
@@ -4,6 +4,26 @@ module MercatorMesonic
     self.table_name = "t022cmp"
     self.primary_key = "c000"
 
+    # --- Class Methods --- #
+
+    def self.import_missing
+      Product.where(photo_file_name: nil).each do |product|
+        file_name = product.number + ".JPG"
+        if bildinstance = self.find_by(c000: file_name)
+          data = StringIO.new(bildinstance.MESOBIN)
+          product.photo = data
+
+          if product.save
+            puts "Speichere Bild: " + file_name
+          else
+            puts product.errors.first
+          end
+        else
+          puts "No photo " + file_name + " found."
+        end
+      end
+    end
+
     # --- Instance Methods --- #
 
     def readonly?  # prevents unintentional changes
