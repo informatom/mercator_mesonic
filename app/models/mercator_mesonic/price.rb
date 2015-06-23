f89c30d6b0b8fd9ecb5b1aed115adb2e42c240a6
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-06-23 13:04:16 +0200

    mesonic price model specs

--- app/models/mercator_mesonic/price.rb
+++ app/models/mercator_mesonic/price.rb
@@ -16,17 +16,22 @@ module MercatorMesonic
     scope :for_date, ->(date) { where(" ( ( t043.c004 IS NULL  OR t043.c004 <= ? ) AND ( t043.c005 IS NULL OR t043.c005 >= ? ) )", date, date ) }
 
     # find customer-group specific price (for a given account_number)
+    # customer group is derived by [t054].[c072] aka KontenstammFakt.kundengruppe
+    # not used anyways...
     scope :by_group_through_customer, ->(account_number) do
       joins("INNER JOIN [t054] ON [t054].[mesoyear] = #{AktMandant.mesoyear} AND [t054].[mesocomp] = #{AktMandant.mesocomp}")
-      .where("[t054].[c112] = ?, account_number").where("[t043].[c003] = CAST([t054].[c072] as varchar(12)) ")
+      .where("[t054].[c112] = ?", account_number).where("[t043].[c003] = CAST([t054].[c072] as varchar(12)) ")
     end
 
+    # Just a guess: via t004 allowed payment types are checked, so better name would be by_payment_type_through_customer
+    # not used anyways...
+    # Modified for Test: CStr([t004].[c004]) -> ([t004].[c004])
     scope :group, ->(account_number) do
       joins("INNER JOIN [t004] ON [t004].[mesoyear] = #{AktMandant.mesoyear} AND [t004].[mesocomp] = #{AktMandant.mesocomp} " +
             " INNER JOIN [t054] ON [t054].[mesoyear] = #{AktMandant.mesoyear} AND " +
             "[t054].[mesocomp] = #{AktMandant.mesocomp} AND ([t004].[c001] = [t054].[c077])" +
             "	INNER JOIN [t286] ON (#{AktMandant.mesocomp} = [t286].[mesocomp]) AND " +
-            "(#{AktMandant.mesoyear} = [t286].[mesoyear]) AND (CStr([t004].[c004]) = [t286].[c000]) ")
+            "(#{AktMandant.mesoyear} = [t286].[mesoyear]) AND (([t004].[c004]) = [t286].[c000]) ")
       .where("[t054].[c112] = ?", account_number)
     end
 
