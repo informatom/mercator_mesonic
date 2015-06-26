52000a18b5d4340c6306ffa03be0c8cd432ec280
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-06-26 09:47:27 +0200

    merctator mesonic inventory extension specs

--- app/models/inventory_extensions.rb
+++ app/models/inventory_extensions.rb
@@ -9,12 +9,18 @@ module InventoryExtensions
 
   # --- Instance Methods --- #
 
-  def mesonic_price(customer_id: nil )
-    customer = User.find(customer_id)
-    return nil unless customer && customer.erp_account_nr
+  def mesonic_price(customer_id: nil)
+    @customer = User.find(customer_id)
+    return nil unless @customer && @customer.erp_account_nr
 
-    customer_prices = self.mesonic_prices.by_customer(customer.erp_account_nr)
-    return customer_prices.first.price if customer_prices.any?
+    @customer_prices = self.mesonic_prices.by_customer(@customer.erp_account_nr)
+
+    if @customer_prices && @customer_prices.any?
+      return @customer_prices.first.price
+    else
+      return nil
+    end
+
     # FIXME! divide by tax rate if brutto pricing
 
 #   HAS 20140407 Was not active in Opensteam!
