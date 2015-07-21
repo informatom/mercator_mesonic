f4097ab6c3f6ab05adc71bc3d41c669990c979ed
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-07-21 09:26:48 +0200

    prices updateable per product

--- app/models/mercator_product_extensions.rb
+++ app/models/mercator_product_extensions.rb
@@ -6,17 +6,21 @@ module MercatorProductExtensions
     def self.reimport_prices
       Product.active.each_with_index do |product, index|
         puts index.to_s + ": " + product.number
-        product.prices.each do |price|
-          price.delete
-        end
-        product.inventories.destroy_all
-
-        MercatorMesonic::Webartikel.where(Preisart: "1")
-                                   .where(Artikelnummer: product.number).each do |webartikel|
-          @inventory = webartikel.create_inventory(product: product)
-          webartikel.create_price(inventory: @inventory)
-        end
+        product.reimport_prices()
       end
+    end
+  end
+
+  def reimport_prices
+    prices.each do |price|
+      price.delete
+    end
+    inventories.destroy_all
+
+    MercatorMesonic::Webartikel.where(Preisart: "1")
+                               .where(Artikelnummer: number).each do |webartikel|
+      @inventory = webartikel.create_inventory(product: self)
+      webartikel.create_price(inventory: @inventory)
     end
   end
 end
