f1f3e1ce332e0b881e406aab295f19072073ce6b
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-08-12 13:17:05 +0200

    price check now also fixis prices

--- app/models/mercator_product_extensions.rb
+++ app/models/mercator_product_extensions.rb
@@ -10,9 +10,9 @@ module MercatorProductExtensions
       end
     end
 
-    def self.check_price
+    def self.check_price(fix: false)
       Product.active.each_with_index do |product, index|
-        product.check_price(index: index)
+        product.check_price(index: index, fix: fix)
       end
     end
   end
@@ -30,7 +30,7 @@ module MercatorProductExtensions
     end
   end
 
-  def check_price(index: 0)
+  def check_price(index: 0, fix: false)
     mercator_price = determine_price(amount: 1,
                                      date: Time.now(),
                                      incl_vat: false,
@@ -38,6 +38,16 @@ module MercatorProductExtensions
     mesonic_price = MercatorMesonic::Webartikel.where(Preisart: "1").find_by(Artikelnummer: number).try(:Preis)
     if mesonic_price && (mesonic_price - mercator_price).abs > 0.1
       puts index.to_s + ": " + number + " " + mesonic_price.to_s + " <> " + mercator_price.to_s
+
+      if fix && (inventories.*.prices.count == 1)
+        price = inventories.*.prices.first.first
+        price.update(value: mesonic_price)
+        new_mercator_price = determine_price(amount: 1,
+                                             date: Time.now(),
+                                             incl_vat: false,
+                                             customer_id: User::JOBUSER.id).to_s
+        puts "updated price to:" + new_mercator_price
+      end
     end
   end
 end
