361ffd03ff9b7be26e8ee504c9aba89ab11fa2d0
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-08-12 12:29:29 +0200

    price check

--- app/models/mercator_product_extensions.rb
+++ app/models/mercator_product_extensions.rb
@@ -9,6 +9,13 @@ module MercatorProductExtensions
         product.reimport_prices()
       end
     end
+
+    def self.check_price
+      Product.active.each_with_index do |product, index|
+        puts index.to_s + ": " + product.number
+        product.check_price()
+      end
+    end
   end
 
   def reimport_prices
@@ -22,5 +29,17 @@ module MercatorProductExtensions
       @inventory = webartikel.create_inventory(product: self)
       webartikel.create_price(inventory: @inventory)
     end
+  end
+
+  def check_price
+    mercator_price =  product.determine_price(amount: 1,
+                                              date: Time.now(),
+                                              incl_vat: false,
+                                              customer_id: User::JOBUSER.id)
+    mesonic_price = MercatorMesonic::Webarikel.find_by(Artikelnummer: number).Preis
+    if mesonic_price == mercator_price
+      puts "OK"
+    else
+      puts mesonic_price + " " + mercator_price
   end
 end
