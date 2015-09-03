95d50e6676ae1105db1df3c7c09f40f8876f65d2
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-09-03 14:20:59 +0200

    check price also updates special prices

--- app/models/mercator_product_extensions.rb
+++ app/models/mercator_product_extensions.rb
@@ -35,18 +35,28 @@ module MercatorProductExtensions
                                      date: Time.now(),
                                      incl_vat: false,
                                      customer_id: User::JOBUSER.id)
-    mesonic_price = MercatorMesonic::Webartikel.where(Preisart: "1").find_by(Artikelnummer: number).try(:Preis)
+
+    webartikel = MercatorMesonic::Webartikel.where(Artikelnummer: number, Preisart: "1")
+                                            .where{(preisdatumVON <= Time.now) & (preisdatumBIS >= Time.now)}
+    if webartikel.count == 1
+      mesonic_price = webartikel.Preis
+    else
+      mesonic_price = MercatorMesonic::Webartikel.where(Preisart: "1").find_by(Artikelnummer: number).try(:Preis)
+    end
+
     if mesonic_price && (mesonic_price - mercator_price).abs > 0.1
       puts index.to_s + ": " + number + " " + mesonic_price.to_s + " <> " + mercator_price.to_s
 
-      if fix && (inventories.*.prices.count == 1)
-        price = inventories.*.prices.first.first
-        price.update(value: mesonic_price)
-        new_mercator_price = determine_price(amount: 1,
-                                             date: Time.now(),
-                                             incl_vat: false,
-                                             customer_id: User::JOBUSER.id).to_s
-        puts "updated price to:" + new_mercator_price
+      if fix
+        prices.each do |price|
+          price.update(value: mesonic_price)
+
+          new_mercator_price = determine_price(amount: 1,
+                                               date: Time.now(),
+                                               incl_vat: false,
+                                               customer_id: User::JOBUSER.id).to_s
+          puts "updated price " + price.id.to_s + " for product " +   price.inventory.product.number + " to: " + new_mercator_price
+        end
       end
     end
   end
