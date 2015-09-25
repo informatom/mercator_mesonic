1402b7956d96d9fde2d2be0fe8ff04d4de5a0e8e
Stefan Haslinger <stefan.haslinger@mittenin.at>
2015-09-25 09:46:04 +0200

    price update now depends on erp_identifier

--- app/models/mercator_product_extensions.rb
+++ app/models/mercator_product_extensions.rb
@@ -11,9 +11,15 @@ module MercatorProductExtensions
     end
 
     def self.check_price(fix: false)
+      JobLogger.info("=" * 50)
+      JobLogger.info("Started method: Product.check_price")
+
       Product.active.each_with_index do |product, index|
         product.check_price(index: index, fix: fix)
       end
+
+      JobLogger.info("Finished method: Product.check_price")
+      JobLogger.info("=" * 50)
     end
   end
 
@@ -31,32 +37,21 @@ module MercatorProductExtensions
   end
 
   def check_price(index: 0, fix: false)
-    mercator_price = determine_price(amount: 1,
-                                     date: Time.now(),
-                                     incl_vat: false,
-                                     customer_id: User::JOBUSER.id)
-
-    webartikel = MercatorMesonic::Webartikel.where(Artikelnummer: number, Preisart: "1")
-                                            .where{(preisdatumVON <= Time.now) & (preisdatumBIS >= Time.now)}
-    if webartikel.count == 1
-      mesonic_price = webartikel[0].Preis
-    else
-      mesonic_price = MercatorMesonic::Webartikel.where(Preisart: "1").find_by(Artikelnummer: number).try(:Preis)
-    end
-
-    if mesonic_price && (mesonic_price - mercator_price).abs > 0.1
-      puts index.to_s + ": " + number + " " + mesonic_price.to_s + " <> " + mercator_price.to_s
-
-      if fix
-        prices.each do |price|
-          price.update(value: mesonic_price)
-
-          new_mercator_price = determine_price(amount: 1,
-                                               date: Time.now(),
-                                               incl_vat: false,
-                                               customer_id: User::JOBUSER.id).to_s
-          puts "updated price " + price.id.to_s + " for product " +   price.inventory.product.number + " to: " + new_mercator_price
+    prices.each do |price|
+      webartikel = MercatorMesonic::Webartikel.find_by(mesokey: price.erp_identifier)
+      if webartikel
+        if (price.value - webartikel.Preis).abs > 0.1
+          puts index.to_s + ": " + self.number + " " + webartikel.Preis.to_s + " <> " + price.value.to_s
+          if fix
+            price.update(value: webartikel.Preis)
+            puts "updated price " + price.id.to_s + " for product " + price.inventory.product.number + " to: " + price.value.to_s
+            JobLogger.info("Updated price " + price.id.to_s + " for product " +
+                           price.inventory.product.number + " to: " + price.value.to_s)
+          end
         end
+      else
+        puts "webartikel for mesokey " + price.erp_identifier.to_s + " not found!"
+        JobLogger.warn("webartikel for mesokey " + price.erp_identifier.to_s + " not found!")
       end
     end
   end
